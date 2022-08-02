df3107ee62c8fa0807ce12407cbd799220ef1ea1
amanda-donini <amanda-donini@hotmail.com>
2022-08-02 11:30:35 +0200

    cria desafio de projeto java

---
+++ Desafio-Dio/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente amanda = new Cliente();
+        amanda.setNome("Amanda");
+
+        Conta cc = new ContaCorrente(amanda);
+        Conta poupanca = new ContaPoupanca(amanda);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
